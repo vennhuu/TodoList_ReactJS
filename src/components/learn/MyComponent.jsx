9ecad0c9c34cd77ffbaa82b533c59ca196d5f086
vennhuu <phuochuu0512@gmail.com>
2025-11-17 17:46:09 +0700

    type

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-
+import './style.css';
 // component = html + css + js
 const MyComponent = () => {
+    // const venn = "Bla bka";
+    // const venn = 123;
+    // const venn = true;
+    const venn = [1, 2, 3];
+    // const venn = {
+    //     name: "hehe",
+    //     age: 20
+    // };
     return (
-        <div> Phan Hữu Phước & vennhuu update </div>
+        <>
+            <div> {JSON.stringify(venn)} Phan Hữu Phước & vennhuu update </div>
+            <div>{console.log("Bùm")}</div>
+            <div className="child" style={{ borderRadius: "10px" }}>Hello</div>
+        </>
     );
 }
 
